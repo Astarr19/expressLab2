8a2ce906613a9fbde627b466d8f1d85de61a0e14
Aidan Starr <aidan.starr19@gmail.com>
2020-08-20 22:55:16 -0400

    post and delete working

--- cart.js
+++ cart.js
@@ -5,16 +5,30 @@ const pool = require('./connection');
 
 function getTable(filters) {
     let myFilters = {...filters};
-    let query = "select * from shopping_cart";
+    let query = "SELECT * FROM shopping_cart";
     let where = [];
     let params = [];
     if (myFilters.id) {
         params.push(myFilters.id);
-        where.push(`id = $${params.length}::integer`);
+        where.push(`id = {$${params.length}}::integer`);
+    }
+    if (myFilters.maxPrice) {
+        params.push(myFilters.maxPrice);
+        where.push(`price <= $${params.length}::integer`)
+    }
+    if (myFilters.prefix) {
+        myFilters.prefix += '%';
+        params.push(myFilters.prefix);
+        where.push(` product LIKE $${params.length}::character varying`)
     }
     if (where.length) {
         query += ' WHERE ' + where.join(' AND ');
     }
+    if (myFilters.pageSize) {
+        params.push(myFilters.pageSize);
+        query += ` LIMIT $${params.length}::integer`
+    }
+    console.log(query, params);
     return pool.query(query,params)
 };
 
@@ -41,10 +55,37 @@ cart.get("/:id", (req,res)=>{
         } else {
             res.sendStatus(404);
         }
+    }).catch(error=>{
+        console.log(error);
+        res.sendStatus(500);
     });
 })
-cart.post("/", (req,res)=>{});
-cart.delete("/:id", (req,res)=>{});
+cart.post("/", (req,res)=>{
+    if (req.body && req.body.product && req.body.price && req.body.quantity) {
+        let values = [
+            req.body.product,
+            req.body.price,
+            req.body.quantity
+        ];
+        pool.query("INSERT INTO shopping_cart (product, price, quantity) VALUES ($1::text, $2::numeric, $3::numeric)", values)
+        .then(()=>{
+            res.status(201);
+            res.json(values);
+        }
+        ).catch(error=>{
+            console.log(error);
+            res.sendStatus(500);
+        })
+    }
+});
+cart.delete("/:id", (req,res)=>{
+    pool.query("DELETE FROM shopping_cart WHERE id=$1::numeric", [req.params.id]).then(()=>{
+        res.sendStatus(204);
+        }).catch(error=>{
+            console.log(error);
+            res.sendStatus(500);
+        });
+});
 cart.put("/:id", (req,res)=>{});
 
 module.exports = cart;
